1b5b75c86ad68622eae5a53b89d6c7cf2e5ad178
lilyeych <lily.eych@gmail.com>
2016-06-13 16:59:52 -0400

    Setup Susy Grid, and header image

---
+++ _site/config.rb
@@ -0,0 +1,21 @@
+# Require any additional compass plugins here.
+require "susy"
+# require "breakpoint"
+
+# Set this to the root of your project when deployed:
+project_type = :stand_alone
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/scss"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
