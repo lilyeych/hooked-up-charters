efe841aedebf2158222042d541747d1d9bba1d11
lilyeych <lily.eych@gmail.com>
2016-06-16 16:27:07 -0400

    Homepage Desktop Layout

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,6 +1,21 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
-	$('#nav-icon').on('click', function(){
-		$('#header-nav').toggleClass('nav-open');
-	});
-});
+$(window).on('resize',mobileNav);
+$(document).ready(mobileNav);
+
+function mobileNav() {
+    if ($(window).width() <= 1024) {
+		$('#nav-icon').on('click', function(){
+			$('#header-nav').toggleClass('nav-open');
+		});
+	}
+}
+
+$(window).on('resize',teaserHeight);
+$(document).ready(teaserHeight);
+
+function teaserHeight() {
+    if ($(window).width() >= 768) {
+
+		var teaserHeight = ($(".teaser-img").outerHeight(true));
+		$('.teaser-text-container').css("height",teaserHeight);
+	}
+}
